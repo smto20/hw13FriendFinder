eddf38e23b836d5ecd1d7f66cc2e6abeece72e57
224Place <224place@gmail.com>
2017-03-13 23:34:54 -0400

    Uploading files have completed

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+//array will take users response
+var friendsArray =
+  [
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+ {
+  "name":"Tom",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      4,
+      4,
+      1,
+      3,
+      2,
+      3
+    ]
+}
+{
+  "name":"Sam",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      4,
+      3,
+      2,
+      2,
+      1,
+      5
+    ]
+}
+  ];
+  module.exports = (friendsArray);
---
+++ server.js
@@ -0,0 +1,25 @@
+//require
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = process.env.PORT || 8889;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+var staticContentFolder = __dirname + "/public";
+app.use(express.static(staticContentFolder));
+
+
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
